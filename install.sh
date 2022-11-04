8bbc5d171383453d8d532e0bbb9d64fb6fd3d4a5
Exiles.prx <campbell.andrew86@yahoo.com>
2022-11-04 12:23:14 -0400

    Script to install starship

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Install fonts
+cd ~
+mkdir .fonts
+which wget || sudo pacman -S wget
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d ~/.fonts
+rm -rf JetBrainsMono.zip
+
+
+# Install starship
+sudo pacman -S starship
+echo 'eval "$(starship init bash)"' >> .bashrc
+
+
+# Add preset
+[[ ! -f ~/.config/starship.toml ]] && touch ~/.config/starship.toml
+starship preset nerd-font-symbols > ~/.config/starship.toml
+
+
+# Source .bashrc file
+source .bashrc
